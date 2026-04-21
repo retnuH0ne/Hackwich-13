9b6535f04f9b2e3ab1e6eb9d1902f4d47632d810
Hunter Trujillo <megafan.exe@gmail.com>
2026-04-21 11:39:50 -1000

    Completed part 1 of geuss the number game.

--- Hackwich 13/ViewController.swift
+++ Hackwich 13/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Float = 0
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        currentValue = slider.value
     }
 
-
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any)
+    {
+        let message = "The value is: \(currentValue)"
+        
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        
+                
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    @IBAction func sliderHasMoved(_ sender: Any)
+    {
+        
+        print("The value of the slider is:  \(slider.value)")
+        currentValue = slider.value
+        
+    }
+        
 }
 
