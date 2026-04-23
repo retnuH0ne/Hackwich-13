3d8a110c6d3b94b251fbe147d67814860ede8a72
Hunter Trujillo <megafan.exe@gmail.com>
2026-04-23 12:05:03 -1000

    Completed problem set and Part 3.

--- Hackwich 13/ViewController.swift
+++ Hackwich 13/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var slider: UISlider!
     var currentValue: Float = 0
     var targetValue: Int = 0
+    @IBOutlet weak var targetLabel: UILabel!
+    
 
     override func viewDidLoad()
     
@@ -21,23 +23,29 @@ class ViewController: UIViewController {
         currentValue = slider.value
         
         startAgain()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+
     }
 
     
     @IBAction func myGuessButtonPressed(_ sender: Any)
     {
-        let message = "The value is: \(currentValue) \nThe target value is \(targetValue)"
+        let message = "Your guess is: \(currentValue) \nThe target value for this round was: \(targetValue)"
         
-        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: "Guess the Number Game", message: message, preferredStyle: .alert)
         
                 
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "New Round", style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
         
         startAgain()
+        
+        
     }
     
     
@@ -53,6 +61,12 @@ class ViewController: UIViewController {
     {
         targetValue = Int.random(in: 1...100)
         currentValue = slider.value
+        updateTargetLabel()
+    }
+    
+    func updateTargetLabel()
+    {
+        targetLabel.text = "\(targetValue)"
     }
 }
 
